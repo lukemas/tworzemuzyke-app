edb0796fe522dae0ad8ce461d546226e6f20bb73
Łukasz Masłowski <l.maslowski91@gmail.com>
2023-08-17 22:45:27 +0200

    login screen view

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+    
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+    
+        if (Auth::attempt($credentials)) {
+            // Authentication passed...
+            return redirect()->intended('/dashboard'); // Change to your desired route after login
+        }
+    
+        return back()->withErrors(['email' => 'Invalid credentials']);
+    }
+}
